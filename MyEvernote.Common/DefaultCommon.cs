34462e75aa9919d516c17237d71f9b5a1118ccd6
Tugay <tugaykosekahya@outlook.com>
2017-09-24 23:29:20 +0300

    Add files via upload

---
+++ MyEvernote.Common/DefaultCommon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyEvernote.Common
+{
+    public class DefaultCommon : ICommon
+    {
+        public string GetCurrentUserName()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUserName()
+        {
+            return "System";
+        }
+
+    }
+
+}
